28563d3d120d6d41cba3a33c026c7cc65a18ea84
Umar Thodge <54655678+umarthodge2@users.noreply.github.com>
2022-09-30 13:19:30 +0530

    Add files via upload

---
+++ array.js
@@ -0,0 +1,22 @@
+<html>
+	<head>
+		<title>
+		</title>
+	</head>
+		<body>
+			<script>
+			var subject=["JAVA","PHP","JAVASCRIPT"];
+			var cars=new Array("BMW","VOLVO","TATA");
+			document.getElementById("demo").innerHTML=subject[1]
+			/*
+			for(var i=0;i<subject.length;i++)
+			{
+				document.write(subject[i]+"</br>")
+			}
+			document.write("=============================<br>")
+			*/
+			</script>
+		
+		<p id="demo"></p>
+		</body>
+</html>
